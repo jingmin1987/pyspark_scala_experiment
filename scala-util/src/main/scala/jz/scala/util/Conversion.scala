3f46d4faacf22e63b1b795d1c3eb22f14fc64013
Jingmin Zhang <jingmin1987@gmail.com>
2022-07-22 22:35:19 -0400

    downgraded scala sdk to 2.12, recomplied jar

--- scala-util/src/main/scala/jz/scala/util/Conversion.scala
+++ scala-util/src/main/scala/jz/scala/util/Conversion.scala
@@ -2,11 +2,11 @@ package jz.scala.util
 
 import java.util
 import java.util.{List => JList}
-import java.util.{HashMap => JMap}
-import scala.jdk.CollectionConverters._
+import java.util.{Map => JMap}
+import scala.collection.JavaConverters._
 
 object Conversion {
-  def toSeq[A](pythonList: JList[A]): Seq[A] = pythonList.asScala.toSeq
+  def toSeq[A](pythonList: JList[A]): Seq[A] = pythonList.asScala
   def toMap[A, B](pythonMap: JMap[A, B]): Map[A, B] = pythonMap.asScala.toMap
 
   def testSeq():Seq[Int] = {
@@ -18,12 +18,15 @@ object Conversion {
   }
 
   def testMap(): Map[String, Int] = {
-    val javaMap = new JMap[String, Int] {
+    val javaMap = new java.util.HashMap[String, Int] {
       put("first", 1)
       put("second", 2)
     }
     toMap(javaMap)
   }
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
+    println(testSeq())
+    println(testMap())
+  }
 }
