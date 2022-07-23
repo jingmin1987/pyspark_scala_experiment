63d55468bc2af84184c68e76b96518faaa4dc4ed
Jingmin Zhang <jingmin1987@gmail.com>
2022-07-22 22:35:37 -0400

    bridge working

--- estimator/bridge.py
+++ estimator/bridge.py
@@ -1,13 +1,36 @@
-from py4j.java_gateway import java_import
 from pyspark.sql import SparkSession
 
 
 class JVMConnection:
-    _active_jvm = None
+
+    _active_connection = None
+
+    @classmethod
+    def set_spark(cls, spark):
+        if cls._active_connection:
+            # Log warning
+            pass
+        else:
+            assert isinstance(spark, SparkSession)
+            cls._active_connection = cls(spark)
 
     @classmethod
     def get_active(cls):
-        if not cls._active_jvm:
+        if not cls._active_connection:
             spark = SparkSession.builder.getOrCreate()
-            cls._active_jvm = spark._jvm
-        return cls._active_jvm
+            cls._active_connection = cls(spark)
+        return cls._active_connection
+
+    def __init__(self, spark=None):
+        if not spark:
+            spark = SparkSession.builder.getOrCreate()
+
+        self.spark = spark
+
+    @property
+    def jvm(self):
+        return self.spark._jvm
+
+    @property
+    def util(self):
+        return self.jvm.jz.scala.util
