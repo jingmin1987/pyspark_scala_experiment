655abb1263345b69ffbfe23e96f279974ba5a872
Jingmin Zhang <jingmin1987@gmail.com>
2022-07-23 12:27:13 -0400

    added cmd() func and docstrings

--- estimator/bridge.py
+++ estimator/bridge.py
@@ -8,11 +8,19 @@ INSTALLED_MODELS = {
 
 
 class JVMConnection:
+    """
+    A class manages the connection between python runtime and spark JVM
+    """
 
     _active_connection = None
 
     @classmethod
-    def set_spark(cls, spark):
+    def set_spark(cls, spark: SparkSession):
+        """
+        Sets the global spark session for JVMConnection. Technically user should only set it once
+        :param spark: A SparkSession object
+        :return:
+        """
         if cls._active_connection:
             # Log warning
             pass
@@ -22,12 +30,20 @@ class JVMConnection:
 
     @classmethod
     def get_active(cls):
+        """
+        Returns the currently active JVMConnection. If no active session available, it will create one
+        :return:
+        """
         if not cls._active_connection:
             spark = SparkSession.builder.getOrCreate()
             cls._active_connection = cls(spark)
         return cls._active_connection
 
     def __init__(self, spark=None):
+        """
+        Initializes an instance of JVMConnection. It shouldn't be called by user
+        :param spark: A SparkSession object
+        """
         if not spark:
             spark = SparkSession.builder.getOrCreate()
 
@@ -42,3 +58,14 @@ class JVMConnection:
     @property
     def util(self):
         return self.jvm.jz.scala.util.Conversion
+
+    def cmd(self, script: str):
+        """
+        A utility function to run command line inside JVM. Mostly for debugging purposes
+        :param script: a command such as `where is python`
+        :return: None
+        """
+        process = self.jvm.Runtime.getRuntime().exec(script).getInputStream()
+        buffer = self.jvm.io.BufferedReader(self.jvm.io.InputStreamReader(process))
+        while self.jvm.System.out.println(buffer.readLine()):
+            pass
